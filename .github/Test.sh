4cf0996432be82d526d505196136bdb4c493da9a
Chấm <111698740+chamchamfy@users.noreply.github.com>
2023-05-12 16:31:30 +0700

    test Test.sh

--- .github/Test.sh
+++ .github/Test.sh
@@ -58,7 +58,7 @@ fi
 
 #Taidulieu
 . $TOME/Option.md
-. mod
+. $TOME/.github/bin/mod
 [[ -n "$systemfc" ]] && sudo bash -c "echo '$systemfc' >> $TOME/tmp/system_file_contexts"
 cat $TOME/tmp/system_file_contexts
 
