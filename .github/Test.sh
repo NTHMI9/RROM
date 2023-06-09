2d2914975152b6f0c434103363952d6b71f456fe
Chấm <111698740+chamchamfy@users.noreply.github.com>
2023-06-09 14:28:53 +0700

    test Test.sh

--- .github/Test.sh
+++ .github/Test.sh
@@ -25,7 +25,7 @@ pip3 install -r requirements.txt > /dev/null
 echo "- Chạy thử nghiệm lệnh"
 
 TOME="$GITHUB_WORKSPACE"
-sudo mkdir $TOME/tmp/vendor
-sudo touch $TOME/tmp/vendor/build.prop
-sudo echo 'import /vendor/${ro.boot.product.hardware.sku}.build.prop' >> $TOME/tmp/vendor/build.prop
-cat $TOME/tmp/vendor/build.prop
+sudo mkdir $TOME/vendor
+sudo touch $TOME/vendor/build.prop
+sudo echo 'import /vendor/${ro.boot.product.hardware.sku}.build.prop' >> $TOME/vendor/build.prop
+cat $TOME/vendor/build.prop
