4798c7d615c40abeec898b05a1c88b7606c2a7f3
Kakathic <143571950+Zelooooo@users.noreply.github.com>
2023-10-29 10:29:13 +0700

    Update Chay.sh

--- .github/Chay.sh
+++ .github/Chay.sh
@@ -23,6 +23,13 @@ docopt
 protobuf<=3.20.1" > requirements.txt
 pip3 install -r requirements.txt > /dev/null
 
+sudo cp /usr/share/zoneinfo/Asia/Ho_Chi_Minh /etc/localtime
+User="User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"
+Xem () { curl -s -G -L -N -H "$User" --connect-timeout 20 "$1"; }
+Turl1="https://github.com$(Xem 'https://github.com/chamchamfy/RROM/issues' | grep -m1 'Link to Issue' | grep -o 'Xây dựng ROM.*"' | cut -d '"' -f3)"
+URL="$(Xem "$Turl1" | grep -m1 'dir="auto">Url:' | grep -o 'Url:.*<' | awk '{print $2}' | cut -d '<' -f1)"
+
+
 echo
 
 Rebuild; 
