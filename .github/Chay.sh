8d8aff725e16bd06b497f993ada6be3d67f9d6d4
Kakathic <143571950+Zelooooo@users.noreply.github.com>
2023-10-29 16:22:09 +0700

    Update Chay.sh

--- .github/Chay.sh
+++ .github/Chay.sh
@@ -14,26 +14,6 @@ echo "▼ Tên máy chủ"
 uname -a
 echo
 
-sudo apt-get update > /dev/null
-sudo apt-get install curl zstd binutils e2fsprogs erofs-utils simg2img img2simg zipalign f2fs-tools p7zip > /dev/null
-pip3 install protobuf bsdiff4 six crypto construct google docopt pycryptodome > /dev/null
-
-echo "pycryptodome
-docopt
-protobuf<=3.20.1" > requirements.txt
-pip3 install -r requirements.txt > /dev/null
-
-sudo cp /usr/share/zoneinfo/Asia/Ho_Chi_Minh /etc/localtime
-User="User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"
-Xem () { curl -s -G -L -N -H "$User" --connect-timeout 20 "$1"; }
-Turl1="https://github.com$(Xem 'https://github.com/chamchamfy/RROM/issues' | grep -m1 'Link to Issue' | grep -o 'Xây dựng ROM.*"' | cut -d '"' -f3)"
-Xem "$Turl1" > $TOME/1.ht
-export URL="$(grep -m1 'dir="auto">Url:' $TOME/1.ht | grep -o 'Url:.*<' | cut -d '"' -f2)"
-export SIZE="$(grep -o 'dir="auto">.*GB' $TOME/1.ht | cut -d '>' -f2 | sed 's|GB||')"
-
-echo
-echo "NEMEROM=${URL##*/}" >> $GITHUB_ENV
-
 Rebuild; 
 
 echo "- Kết thúc"
