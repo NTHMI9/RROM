a8159d52bb3e22b679441ab37078fd7939e82cba
Kakathic <143571950+Zelooooo@users.noreply.github.com>
2023-10-30 21:18:08 +0700

    Update Upload.sh

--- .github/Upload.sh
+++ .github/Upload.sh
@@ -1,17 +1,26 @@
 # kakathic
 TOME="$GITHUB_WORKSPACE"
 cd $TOME/.github/libpy/Flash2in1
+GITENV(){ [ "$2" ] || ( echo "- Error: $1" && exit 0 ); echo "$1=$2" >> $GITHUB_ENV; eval "$1=$2"; }
 
 if [ -e $TOME/ok ]; then
+# Nén rom zip
 echo "Tên ROM: $NEMEROM"
 zip -qr $TOME/$NEMEROM *
+
 url2="$(curl -s https://api.gofile.io/getServer | jq -r .data.server)"
 eval "curl -F 'file=@$TOME/$NEMEROM' 'https://$url2.gofile.io/uploadFile' > $TOME/1.json"
+GITENV LINKROM "$(cat $TOME/1.json | jq -r .data.downloadPage)"
+
+# Link download 
 echo
-echo "LINKROM=$(cat $TOME/1.json | jq -r .data.downloadPage)" | tee -a $GITHUB_ENV
-echo "MD5=$(cat $TOME/1.json | jq -r .data.md5)" | tee -a $GITHUB_ENV
-echo "NAMER=$(cat $TOME/1.json | jq -r .data.fileName)" | tee -a $GITHUB_ENV
+echo "Link download: $LINKROM"
+
+GITENV THATBAI "Tạo rom thành công <br/><br/>Link Download: ${{ env.LINKROM }}"
+GITENV THETB "Hoàn thành"
+
 else
 echo '- Tạo ROM thất bại'
-
+GITENV THATBAI "Tạo rom thất bại, Xem log: [📱Actions](https://github.com/chamchamfy/RROM/actions)"
+GITENV THETB "Thất bại"
 fi
