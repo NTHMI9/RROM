45097f76a8ec1f647b9b94e612cbde0a9c629da7
Kakathic <143571950+Zelooooo@users.noreply.github.com>
2023-10-30 21:44:40 +0700

    Update Taive.sh

--- .github/Taive.sh
+++ .github/Taive.sh
@@ -9,12 +9,12 @@ sudo cp /usr/share/zoneinfo/Asia/Ho_Chi_Minh /etc/localtime
 User="User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"
 Xem () { curl -s -G -L -N -H "$User" --connect-timeout 20 "$1"; }
 Taive () { curl -L -N -H "$User" --connect-timeout 20 "$1" -o "$2"; }
-GITENV(){ [ "$2" ] || ( echo "- Error: $1" && exit 0 ); echo "$1=$2" >> $GITHUB_ENV; eval "$1=$2"; }
+GITENV(){ [ "$2" ] || ( echo "- Error: $1"); echo "$1=$2" >> $GITHUB_ENV; eval "$1='$2'"; }
 
 # CÁC TÙY CHỌN WEB
 
 # link url rom và size
-Turl1="https://github.com$(Xem 'https://github.com/chamchamfy/RROM/issues?q=is%3Aissue+is%3Aclosed' | grep -m1 'Link to Issue' | grep -o 'Xây dựng ROM.*"' | cut -d '"' -f3)"
+Turl1="https://github.com$(Xem 'https://github.com/chamchamfy/RROM/issues' | grep -m1 'Link to Issue' | grep -o 'Xây dựng ROM.*"' | cut -d '"' -f3)"
 Xem "$Turl1" > $TOME/1.ht
 URLKK="$(grep -m1 'dir="auto">Url:' $TOME/1.ht | grep -o 'Url:.*<' | cut -d '"' -f2)"
 SIZEKK="$(grep -o 'dir="auto">.*GB' $TOME/1.ht | cut -d '>' -f2 | sed 's|GB||')"
