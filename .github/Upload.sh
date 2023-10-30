a76e6e0d61c3db150e6f01493d7c510a0b266a05
Kakathic <143571950+Zelooooo@users.noreply.github.com>
2023-10-30 21:06:48 +0700

    Update Upload.sh

--- .github/Upload.sh
+++ .github/Upload.sh
@@ -1,1 +1,17 @@
+# kakathic
+TOME="$GITHUB_WORKSPACE"
+cd $TOME/.github/libpy/Flash2in1
 
+if [ -e $TOME/ok ]; then
+echo "Tên ROM: $NEMEROM"
+zip -qr $TOME/$NEMEROM *
+url2="$(curl -s https://api.gofile.io/getServer | jq -r .data.server)"
+eval "curl -F 'file=@$TOME/$NEMEROM' 'https://$url2.gofile.io/uploadFile' > $TOME/1.json"
+echo
+echo "LINKROM=$(cat $TOME/1.json | jq -r .data.downloadPage)" | tee -a $GITHUB_ENV
+echo "MD5=$(cat $TOME/1.json | jq -r .data.md5)" | tee -a $GITHUB_ENV
+echo "NAMER=$(cat $TOME/1.json | jq -r .data.fileName)" | tee -a $GITHUB_ENV
+else
+echo '- Tạo ROM thất bại'
+
+fi
