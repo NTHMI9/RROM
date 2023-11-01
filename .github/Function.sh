2845f38d5613411cdee38b23226c6e6e12777896
Kakathic <kakathic@gmail.com>
2023-11-01 23:07:24 +0700

    Update Function.sh

--- .github/Function.sh
+++ .github/Function.sh
@@ -8,3 +8,5 @@ GITENV(){ [ "$2" ] || ( echo "- Error: $1"); echo "$1=$2" >> $GITHUB_ENV; eval "
 checktc(){ grep -co 'dir="auto">.*'$1'' $TOME/1.ht 2>/dev/null; }
 checkbox(){ grep -m1 "$1</li>" $TOME/1.ht 2>/dev/null | grep -cm1 'checked=' 2>/dev/null; }
 Chatbot(){ gh issue comment $NUMBIE --body "$1" & echo "$1"; }
+addlabel(){ gh issue edit $NUMBIE --add-label "$1"; }
+closechat(){ gh issue close $NUMBIE -c "$1"; }
