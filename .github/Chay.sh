37b82801d35631d6cb9b5fe19dc61cd40403650c
Kakathic <kakathic@gmail.com>
2023-11-02 15:58:37 +0700

    Update Chay.sh

--- .github/Chay.sh
+++ .github/Chay.sh
@@ -1,11 +1,11 @@
 #kakathic & chamchamfy
+. .github/Function.sh
 
 sudo rm -rf /usr/share/dotnet
 sudo rm -rf /opt/ghc
 sudo rm -rf "/usr/local/share/boost"
 sudo rm -rf "$AGENT_TOOLSDIRECTORY"
 
-export TOME="$GITHUB_WORKSPACE"
 export PATH="$TOME/.github/bin:$PATH"
 chmod -R 777 .github/bin/* >/dev/null
 chmod -R 777 .github/*.sh >/dev/null
